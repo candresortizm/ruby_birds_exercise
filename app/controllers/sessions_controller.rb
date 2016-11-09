252a5f1563d0863a5605110dcd8947064bf83af5
Andres Ortiz <candresortizm@gmail.com>
2016-11-08 21:57:17 -0500

    crear usuario

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
-
+include SessionsHelper
 
   def new
   end
 
   def create
     user = User.find_by_email(sessions_params[:email])
-    if user and user.authenticate(sessions_params[:password])
+    if user and user.authenticate(sessions_params[:password]) #authenticate es delhas_secure_password
       login user
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 
   def delete
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new(first_name: "Jorge", last_name: "Gomez")
+    @user = User.new
   end
 
   def create
@@ -8,8 +8,12 @@ class UsersController < ApplicationController
     if @user.save
       login(@user)
       redirect_to root_path
+    else
+      redirect_to new_users_path
     end
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:nickname, :email, :first_name, :last_name, :password, :password_confirmation)
